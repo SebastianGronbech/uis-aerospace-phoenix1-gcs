74e963e67ec5c78477184e2fbad3761ef235786d
Sebastian Grønbech <115935487+SebastianGronbech@users.noreply.github.com>
2025-03-14 11:09:02 +0100

    Add Tenant aggregate root

---
+++ Gui/src/Core/Domain/Tenant/Tenant.cs
@@ -0,0 +1,26 @@
+namespace Core.Domain.Tenant;
+
+public class Tenant
+{
+    public Guid Id { get; protected set; }
+    public string Name { get; private set; }
+
+    private List<TenantUser> _tenantUsers = new();
+    public IReadOnlyCollection<TenantUser> TenantUsers => _tenantUsers.AsReadOnly();
+
+    public DateTimeOffset CreatedAt { get; protected set; }
+    public DateTimeOffset? UpdatedAt { get; private set; }
+    public DateTimeOffset? DeletedAt { get; protected set; }
+
+    public Tenant(string name)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        CreatedAt = DateTimeOffset.UtcNow;
+    }
+
+    public void AddTenantUser(Guid tenantUserId)
+    {
+        _tenantUsers.Add(new TenantUser(tenantUserId));
+    }
+}
